e124ba12258eafa75935b02d75426c4021720fb5
floa05 <88208225+floa05@users.noreply.github.com>
2022-08-29 13:15:23 -0500

    bitbucket

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "example" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, World! This is Larissa's Terraform project" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
   user_data_replace_on_change = true
@@ -43,3 +43,4 @@ output "security_group_id" {
   value =aws_security_group.instance.id
   description = "The Id of the security group"
 }
+
